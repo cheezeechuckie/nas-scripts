201eaf779e5ea6f10a9654ebd611c91c963b40a5
cheezeechuckie <71899061+cheezeechuckie@users.noreply.github.com>
2021-01-28 15:34:10 -0500

    Create sendsmart.sh

---
+++ sendsmart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Place this in /bin/
+# Call: sh /bin/sendsmart.sh
+# Credit: started from tkaiser on omv forum
+
+
+TempFile=$(mktemp /tmp/${0##*/}.XXXXXX)
+trap "rm -f \"${TempFile}\" ; exit 0" 0 1 2 3 15
+(
+echo "To: charlesfung@gmail.com"
+echo "Subject: SMART Results for all drives"
+echo " "
+) > ${TempFile}
+for disk in /dev/sd? ; do
+        smartctl -a ${disk} >> ${TempFile}
+done
+sendmail -t < ${TempFile}
+exit 0
+
+# -a - all attributes
